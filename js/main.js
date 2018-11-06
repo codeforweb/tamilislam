ce8fd8585530e95c9f8433fa20f87aba58fe3bf4
Khan <abuazraa@gmail.com>
2018-11-06 19:09:23 +0530

    Language switching

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-
-async function compileAndRender(source, compiledVar, jsonFile, outputEl){
-	await compile(source, compiledVar)
-	await renderTemplate(jsonFile, compiledVar, outputEl)
+async function fetchAsync(jsonFile) {
+	let data = await (await fetch(jsonFile)).json();
+	return data;
+}
+async function compileAndRender(source, compiledVar, jsonFile, outputEl, langCode, surahno, ayath){
+	await compile(source, compiledVar,surahno)
+	var result = await fetchAsync (jsonFile);
+	await renderTemplate(result, compiledVar, outputEl, langCode, surahno, ayath)
 }
 function compile(source, compiledVar) {
 	var compiled = dust.compile(source, compiledVar);
 	dust.loadSource(compiled);
-	// console.log(source,compiledVar)
 }
-
-function renderTemplate(jsonFile, compiledVar, outputEl) {
-	fetch(jsonFile)
-	.then(response => {
-		 return response.json();
-	})
-	.then(function(result) {
-		// console.log(result)
+function renderTemplate(result, compiledVar, outputEl,langCode, surahno, ayath) {
+		result["langCode"] = langCode
+		if(surahno) result["surahno"] = surahno
+		if(ayath) result["ayath"] = ayath		
 		dust.render(compiledVar, result, function (err, out) {
+
 			document.querySelector(outputEl).innerHTML = document.getElementById('tblcontainer').innerHTML + out
 		});
-	})
+
+}
+function changeLang(){
+	var elem = (typeof this.selectedIndex === "undefined" ? window.event.srcElement : this);
+    var value = elem.value || elem.options[elem.selectedIndex].value;
+    console.log(value);
 }
